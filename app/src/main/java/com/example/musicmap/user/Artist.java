6dd15139633934f6397896a1883749061f08989c
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2023-02-21 11:32:29 +0100

    Updated the User & Artist classes

--- app/src/main/java/com/example/musicmap/user/Artist.java
+++ app/src/main/java/com/example/musicmap/user/Artist.java
@@ -1,5 +1,15 @@
 package com.example.musicmap.user;
 
-public class Artist extends User {
+import java.util.UUID;
 
+public class Artist extends User {
+    public Artist(User user) {
+        super(user.getUsername(), user.getFirstName(), user.getLastName(), user.getEmail(), user.getUuid());
+
+    }
+
+    public Artist(String username, String firstName, String lastName, String email, UUID uuid) {
+        super(username, firstName, lastName, email, uuid);
+
+    }
 }
--- app/src/main/java/com/example/musicmap/user/User.java
+++ app/src/main/java/com/example/musicmap/user/User.java
@@ -3,15 +3,26 @@ package com.example.musicmap.user;
 import java.util.UUID;
 
 /**
- *  The user.
+ * The user.
  */
 public class User {
     private String username;
+    private String firstName;
+    private String lastName;
     private String email;
     private UUID uuid;
 
+    public User(String username, String firstName, String lastName, String email, UUID uuid) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.uuid = uuid;
+    }
+
     /**
      * This method retrieves the username of the user.
+     *
      * @return the username of the user
      */
     public String getUsername() {
@@ -19,7 +30,26 @@ public class User {
     }
 
     /**
+     * This method retrieves the first name (given name) of the user.
+     *
+     * @return the first name of the user
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * This method retrieves the last name (family name) of the user.
+     *
+     * @return the last name of the user
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
      * This method retrieves the email of the user.
+     *
      * @return the email of the user
      */
     public String getEmail() {
@@ -28,6 +58,7 @@ public class User {
 
     /**
      * This method retrives the uuid of the user.
+     *
      * @return the uuid of the user
      */
     public UUID getUuid() {
