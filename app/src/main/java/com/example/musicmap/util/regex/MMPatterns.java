e8e1d10f38357d92465ed7729e6fb0d2b8b63434
TPGamesNL <pet.teun.03@gmail.com>
2023-03-02 22:16:40 +0100

    Remove line start and end from regex patterns, since using Matcher#matches ensures full string match already

--- app/src/main/java/com/example/musicmap/util/regex/MMPatterns.java
+++ app/src/main/java/com/example/musicmap/util/regex/MMPatterns.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class MMPatterns {
 
-    public static final Pattern USERNAME = Pattern.compile("^\\w{3,25}$");
-    public static final Pattern PASSWORD = Pattern.compile("^.{6,}$");
+    public static final Pattern USERNAME = Pattern.compile("\\w{3,25}");
+    public static final Pattern PASSWORD = Pattern.compile(".{6,}");
 
 }
