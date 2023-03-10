7eb90c65c3ee943402a765bb45f63521b8d0e879
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2023-03-10 14:22:43 +0100

    Added getUserWithEmail() query

--- app/src/main/java/com/example/musicmap/util/firebase/Queries.java
+++ app/src/main/java/com/example/musicmap/util/firebase/Queries.java
@@ -7,9 +7,15 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 public class Queries {
 
-    public static Task<QuerySnapshot> getUsersWithUsername(FirebaseFirestore firestore,
-                                                           String username) {
+    public static Task<QuerySnapshot> getUsersWithUsername(String username) {
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
         Query query = firestore.collection("Users").whereEqualTo("username", username);
+        return query.get();
+    }
+
+    public static Task<QuerySnapshot> getUserWithEmail(String email) {
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        Query query = firestore.collection("Users").whereEqualTo("email", email);
         return query.get();
     }
 
