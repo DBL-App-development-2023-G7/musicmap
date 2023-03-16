ffc0ad481789b65b6c7f93460daf3a0afc3c6e93
TPGamesNL <pet.teun.03@gmail.com>
2023-03-16 17:02:41 +0100

    Fix formatting

--- app/src/main/java/com/example/musicmap/util/permissions/Permission.java
+++ app/src/main/java/com/example/musicmap/util/permissions/Permission.java
@@ -1,10 +1,8 @@
 package com.example.musicmap.util.permissions;
 
-import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.util.Log;
 
 import androidx.activity.result.ActivityResultCaller;
