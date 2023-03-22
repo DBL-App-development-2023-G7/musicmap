cee0725304cb7d07b79aa600cb692711dc20ed8b
TPGamesNL <pet.teun.03@gmail.com>
2023-03-22 17:19:32 +0100

    Add details to ConcertMemory, allow Firebase POJO serialization & deserialization

--- app/src/main/java/com/example/musicmap/feed/ConcertMemory.java
+++ app/src/main/java/com/example/musicmap/feed/ConcertMemory.java
@@ -1,5 +1,9 @@
 package com.example.musicmap.feed;
 
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+
 import com.google.firebase.firestore.GeoPoint;
 
 import java.util.Date;
@@ -9,16 +13,33 @@ import java.util.Date;
  */
 public class ConcertMemory extends Post {
 
-    // TODO remaining details, e.g. name of concert
-    private final String video;
+    private String name;
+    private String video;
 
-    public ConcertMemory(Date timePosted, GeoPoint location, String video) {
-        super(timePosted, location);
+    @SuppressWarnings("unused")
+    public ConcertMemory() { }
+
+    public ConcertMemory(String authorUid, Date timePosted, GeoPoint location, String name, String video) {
+        super(authorUid, timePosted, location);
+        this.name = name;
         this.video = video;
     }
 
-    public String getVideo() {
-        return video;
+    public String getName() {
+        return name;
+    }
+
+    public Uri getVideo() {
+        return Uri.parse(video);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "ConcertMemory{" +
+                "name='" + name + '\'' +
+                ", video='" + video + '\'' +
+                '}';
     }
 
 }
