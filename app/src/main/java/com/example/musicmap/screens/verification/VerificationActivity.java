42298d145b327a306550e3fe0b8c011c213a1dcb
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2023-03-22 21:22:02 +0100

    Fixed bugs and optimized the VerificationActivity

--- app/src/main/java/com/example/musicmap/screens/verification/VerificationActivity.java
+++ app/src/main/java/com/example/musicmap/screens/verification/VerificationActivity.java
@@ -4,43 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.musicmap.R;
+import com.example.musicmap.SessionListenerActivity;
 import com.example.musicmap.screens.main.HomeActivity;
+import com.example.musicmap.user.Artist;
+import com.example.musicmap.user.Session;
+import com.example.musicmap.util.firebase.AuthSystem;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
-public class VerificationActivity extends AppCompatActivity {
+public class VerificationActivity extends SessionListenerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification);
 
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-
-        //TODO should be moved, changed and optimized <<IGNORE FOR NOW>>
-        DocumentReference docRef = firestore.collection("Users").document(auth.getCurrentUser().getUid());
-        docRef.addSnapshotListener((value, error) -> {
-            boolean verified = (Boolean) value.getData().get("verified");
-            if (verified) {
-                Intent homeIntent = new Intent(this, HomeActivity.class);
-                homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(homeIntent);
-                finish();
-            }
-        });
-
         Button signOutVerificationButton = findViewById(R.id.signout_verification_button);
-        signOutVerificationButton.setOnClickListener(view -> signOut());
+        signOutVerificationButton.setOnClickListener(view -> AuthSystem.logout());
     }
 
-    private void signOut() {
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        auth.signOut();
+    @Override
+    public void onSessionStateChanged() {
+        Artist currentArtist = (Artist) Session.getInstance().getCurrentUser();
+
+        if (currentArtist.isVerified()) {
+            Intent homeIntent = new Intent(this, HomeActivity.class);
+            homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(homeIntent);
+            finish();
+        }
     }
 
 }
