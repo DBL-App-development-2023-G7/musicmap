ba48b907a494447f1cae33001c11f6566a845100
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2023-03-23 20:12:04 +0100

    Implemented back button for the ProfileActivity

--- app/src/main/java/com/example/musicmap/screens/profile/ProfileActivity.java
+++ app/src/main/java/com/example/musicmap/screens/profile/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.musicmap.screens.profile;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.musicmap.R;
 import com.example.musicmap.SessionListenerActivity;
@@ -11,6 +12,9 @@ public class ProfileActivity extends SessionListenerActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        ImageView backButton = findViewById(R.id.appbarBack);
+        backButton.setOnClickListener(view -> super.onBackPressed());
     }
 
 }
