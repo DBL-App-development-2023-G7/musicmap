f331e7e7e7042f3b4dcaa61062e6f8d0308aa1b4
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2023-03-25 10:06:48 +0100

    Added more settings to the profile category

--- app/src/main/java/com/example/musicmap/screens/settings/SettingsFragment.java
+++ app/src/main/java/com/example/musicmap/screens/settings/SettingsFragment.java
@@ -2,8 +2,13 @@ package com.example.musicmap.screens.settings;
 
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
+import com.example.musicmap.BuildConfig;
 import com.example.musicmap.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
@@ -11,5 +16,8 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        Preference versionPreference = preferenceScreen.getPreference(preferenceScreen.getPreferenceCount() - 1);
+        versionPreference.setSummary("The current version of the app is: " + BuildConfig.VERSION_NAME);
     }
 }
