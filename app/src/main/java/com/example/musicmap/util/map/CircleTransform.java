b04a0c8b622e512830ca5793e53c4593011db489
TPGamesNL <pet.teun.03@gmail.com>
2023-03-25 12:12:02 +0100

    Add circle Picasso transformation

---
+++ app/src/main/java/com/example/musicmap/util/map/CircleTransform.java
@@ -0,0 +1,52 @@
+package com.example.musicmap.util.map;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Shader;
+
+import com.squareup.picasso.Transformation;
+
+/**
+ * A transformation that cuts of the corners of the input image,
+ * turning it into a circle.
+ *
+ * Sourced from <a href="https://stackoverflow.com/a/26112408">Stack Overflow</a>.
+ */
+public class CircleTransform implements Transformation {
+
+    @Override
+    public Bitmap transform(Bitmap source) {
+        int size = Math.min(source.getWidth(), source.getHeight());
+
+        int x = (source.getWidth() - size) / 2;
+        int y = (source.getHeight() - size) / 2;
+
+        Bitmap squaredBitmap = Bitmap.createBitmap(source, x, y, size, size);
+        if (squaredBitmap != source) {
+            source.recycle();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(size, size, source.getConfig());
+
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        BitmapShader shader = new BitmapShader(squaredBitmap,
+                Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        paint.setShader(shader);
+        paint.setAntiAlias(true);
+
+        float r = size / 2f;
+        canvas.drawCircle(r, r, r, paint);
+
+        squaredBitmap.recycle();
+        return bitmap;
+    }
+
+    @Override
+    public String key() {
+        return "circle";
+    }
+
+}
