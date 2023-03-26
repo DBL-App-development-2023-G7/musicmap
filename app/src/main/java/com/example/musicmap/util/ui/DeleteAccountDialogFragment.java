443db4f990c4eeccac29cf913f1828f175c72518
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2023-03-26 19:53:52 +0200

    Adjusted the delete account dialog

--- app/src/main/java/com/example/musicmap/util/ui/DeleteAccountDialogFragment.java
+++ app/src/main/java/com/example/musicmap/util/ui/DeleteAccountDialogFragment.java
@@ -6,6 +6,7 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
@@ -26,21 +27,27 @@ public class DeleteAccountDialogFragment extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = requireActivity().getLayoutInflater();
 
-        View view = inflater.inflate(R.layout.delete_accout_dialog, null);
-        passwordInput = view.findViewById(R.id.password_dialog_editText);
+        View dialogView = inflater.inflate(R.layout.delete_accout_dialog, null);
+        passwordInput = dialogView.findViewById(R.id.password_dialog_editText);
 
         // Inflate and set the layout for the dialog
         // Pass null as the parent view because its going in the dialog layout
-        builder.setView(view)
+        AlertDialog alertDialog = builder.setView(dialogView)
                 // Add title to the dialog
                 .setTitle(R.string.delete_account_dialog_title)
                 // Add description message to the dialog
                 .setMessage(R.string.delete_account_dialog_message)
                 // Add action buttons
-                .setPositiveButton(R.string.delete_account, (dialog, id) -> deleteAccount())
-                .setNegativeButton(R.string.cancel, (dialog, id) -> dialog.cancel());
+                .setPositiveButton(R.string.delete_account, null).setNegativeButton(R.string.cancel,
+                        (dialog, id) -> dialog.dismiss()).create();
 
-        return builder.create();
+        // Required so that the dialog won't close when an error occurs after pressing delete account
+        alertDialog.setOnShowListener(dialog -> {
+            Button button = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+            button.setOnClickListener(view -> deleteAccount());
+        });
+
+        return alertDialog;
     }
 
     private void deleteAccount() {
