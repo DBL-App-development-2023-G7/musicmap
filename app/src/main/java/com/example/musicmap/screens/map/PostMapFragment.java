1905de4ac6b1404a70469656221a0b7253090042
TPGamesNL <pet.teun.03@gmail.com>
2023-03-26 20:18:37 +0200

    Make PostMapFragment use existing posts

--- app/src/main/java/com/example/musicmap/screens/map/PostMapFragment.java
+++ app/src/main/java/com/example/musicmap/screens/map/PostMapFragment.java
@@ -1,26 +1,65 @@
 package com.example.musicmap.screens.map;
 
-import com.example.musicmap.feed.MusicMemory;
+import android.util.Log;
+
+import com.example.musicmap.util.firebase.Queries;
 import com.example.musicmap.util.map.MusicMemoryOverlay;
-import com.google.firebase.firestore.GeoPoint;
 
-import java.util.Date;
+import org.osmdroid.views.overlay.FolderOverlay;
+import org.osmdroid.views.overlay.Overlay;
 
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * A map showing posts (music memories etc).
+ */
 public class PostMapFragment extends MapFragment {
 
-    private static final MusicMemory MUSIC_MEMORY = new MusicMemory(
-            "AUTHOR HERE :)",
-            new Date(),
-            new GeoPoint(51.446097, 5.486363),
-            "https://i.imgur.com/5yeBVeM.jpeg",
-            "bazinga"
-    );
+    private static final String TAG = "PostMapFragment";
+
+    private final FolderOverlay postsFolder = new FolderOverlay();
+    private final AtomicBoolean isUpdating = new AtomicBoolean(false);
 
     @Override
     protected void addOverlays() {
         super.addOverlays();
 
-        addOverlay(new MusicMemoryOverlay(getMapView(), MUSIC_MEMORY));
+        addOverlay(postsFolder);
+
+        updatePosts();
+    }
+
+    /**
+     * Updates the posts displayed on the map, possibly adding new posts.
+     */
+    public void update() {
+        if (isUpdating.getAndSet(true)) {
+            updatePosts();
+            isUpdating.set(false);
+        }
+    }
+
+    private void updatePosts() {
+        // Remove all music memories from map
+        for (Overlay overlay : new ArrayList<>(postsFolder.getItems())) {
+            postsFolder.remove(overlay);
+        }
+
+        // Start fetching music memories
+        Queries.getAllMusicMemories().addOnCompleteListener(completedTask -> {
+            if (completedTask.isSuccessful()) {
+                // Add all retrieved music memories to map
+                completedTask.getResult().stream()
+                        .map(musicMemory -> new MusicMemoryOverlay(getMapView(), musicMemory))
+                        .forEach(postsFolder::add);
+
+                // Refresh map
+                getMapView().invalidate();
+            } else {
+                Log.e(TAG, "Exception occurred while getting map music memories", completedTask.getException());
+            }
+        });
     }
 
 }
