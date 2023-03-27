e0519ffc9e84e54e4fb2eadb9771b758dbd6939b
Pradyuman7 <p.dixit@student.tue.nl>
2023-03-27 07:49:01 +0200

    Add ConcertMemory and MusicMemory tests

---
+++ app/src/androidTest/java/com/example/musicmap/util/feed/ConcertMemoryTest.java
@@ -0,0 +1,39 @@
+package com.example.musicmap.util.feed;
+
+import android.net.Uri;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.musicmap.feed.ConcertMemory;
+import com.google.firebase.firestore.GeoPoint;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Date;
+
+@RunWith(AndroidJUnit4.class)
+public class ConcertMemoryTest {
+
+    private ConcertMemory concertMemory;
+
+    @Before
+    public void setUp() {
+        this.concertMemory = new ConcertMemory("author-uid", new Date(),
+                new GeoPoint(10, 10), "name", "video");
+    }
+
+    @Test
+    public void testGetName() {
+        Assert.assertEquals(this.concertMemory.getName(), "name");
+    }
+
+    @Test
+    public void testGetSong() {
+        Assert.assertEquals(this.concertMemory.getVideo(), Uri.parse("video"));
+    }
+
+}
+
---
+++ app/src/androidTest/java/com/example/musicmap/util/feed/MusicMemoryTest.java
@@ -0,0 +1,38 @@
+package com.example.musicmap.util.feed;
+
+import android.net.Uri;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.musicmap.feed.MusicMemory;
+import com.google.firebase.firestore.GeoPoint;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Date;
+
+@RunWith(AndroidJUnit4.class)
+public class MusicMemoryTest {
+
+    private MusicMemory musicMemory;
+
+    @Before
+    public void setUp() {
+        this.musicMemory = new MusicMemory("author-uid", new Date(),
+                new GeoPoint(10, 10), "photo", "song");
+    }
+
+    @Test
+    public void testGetPhoto() {
+        Assert.assertEquals(this.musicMemory.getPhoto(), Uri.parse("photo"));
+    }
+
+    @Test
+    public void testGetSong() {
+        Assert.assertEquals(this.musicMemory.getSong(), "song");
+    }
+
+}
