11bd3a0e01050dad7cece16e9b7ae7ea62ac91c9
Pradyuman7 <p.dixit@student.tue.nl>
2023-03-28 17:19:18 +0200

    Add gif to NoInternetActivity

--- app/src/main/java/com/example/musicmap/screens/NoInternetActivity.java
+++ app/src/main/java/com/example/musicmap/screens/NoInternetActivity.java
@@ -1,9 +1,11 @@
 package com.example.musicmap.screens;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.bumptech.glide.Glide;
 import com.example.musicmap.R;
 
 public class NoInternetActivity extends AppCompatActivity {
@@ -12,5 +14,10 @@ public class NoInternetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_internet);
+
+        ImageView imageView = findViewById(R.id.no_internet_placeholder);
+        String path = "android.resource://" + getPackageName() + "/" + R.raw.no_internet;
+        Glide.with(this).load(path).into(imageView);
     }
+
 }
