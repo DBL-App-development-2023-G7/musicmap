50050289e3455b1338d4b46dbc27e24506b9778e
CodeNamedRobin <robinvandijke2002@gmail.com>
2023-03-29 15:50:19 +0200

    Updated musicmemory fragment - Added all views and widgets - Crated mapView.

--- app/src/main/java/com/example/musicmap/screens/main/MusicMemoryFragment.java
+++ app/src/main/java/com/example/musicmap/screens/main/MusicMemoryFragment.java
@@ -12,6 +12,8 @@ import android.view.ViewGroup;
 import com.example.musicmap.R;
 import com.example.musicmap.feed.MusicMemory;
 
+import org.osmdroid.views.MapView;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link MusicMemoryFragment#newInstance} factory method to
@@ -24,29 +26,13 @@ public class MusicMemoryFragment extends Fragment {
     private String song;
     private String authorUID;
     private String photoURI;
+    private MapView mapView;
 
     public static final String TAG = "MusicMemoryFragment";
 
     // TODO: Rename and change types of parameters
     private MusicMemory musicMemory;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment MusicMemoryFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MusicMemoryFragment newInstance(MusicMemory mm) {
-        MusicMemoryFragment fragment = new MusicMemoryFragment();
-        Bundle args = new Bundle();
-        args.putString("song", mm.getSong());
-        args.putString("photoURL", mm.getPhoto().toString());
-        args.putString("authorUID", mm.getAuthorUid());
-
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,17 +40,18 @@ public class MusicMemoryFragment extends Fragment {
         HomeActivity activity = (HomeActivity) requireActivity();
         this.musicMemory = activity.getCurrentMusicMemory();
 
-        if (getArguments() != null) {
-            this.song = getArguments().getString("song");
-            this.photoURI = getArguments().getString("photoURL");
-            this.authorUID = getArguments().getString("authorUID");
-        }
+        System.out.println(this.musicMemory.getSong());
+        System.out.println(this.musicMemory.getPhoto());
+        System.out.println(this.musicMemory.getLocation());
+        System.out.println(this.musicMemory.getTimePosted());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_music_memory, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_music_memory, container, false);
+        this.mapView = rootView.findViewById(R.id.post_map);
+        return rootView;
     }
 }
