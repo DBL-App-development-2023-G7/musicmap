b14e38ad7f40cf71150dc4adeea66da7226cb7b8
TPGamesNL <pet.teun.03@gmail.com>
2023-03-30 22:25:07 +0200

    Reformat SpotifyData

--- app/src/main/java/com/example/musicmap/util/spotify/SpotifyData.java
+++ app/src/main/java/com/example/musicmap/util/spotify/SpotifyData.java
@@ -5,19 +5,23 @@ import android.util.Log;
 import se.michaelthelin.spotify.SpotifyApi;
 
 /**
- * A storage class which stores some global varibles
+ * A storage class which stores some global variables
  * Using static classes is not ideal but it is easy to do
  */
 public class SpotifyData {
+
     private static final String CLIENT_ID = "56ab7fed83514a7a96a7b735737280d8";
     private static final String REDIRECT_URI = "musicmap://spotify-auth";
-    private static String token= null;
 
+    private static String token = null;
     private static SpotifyApi spotifyApi;
 
-    public static void setToken(String inputToken){
+    public static void setToken(String inputToken) {
         token = inputToken;
-        spotifyApi = new SpotifyApi.Builder().setAccessToken(token).build();
+        spotifyApi = new SpotifyApi.Builder()
+                .setAccessToken(token)
+                .build();
+
         Log.d("debug", String.format("[poop] token recieved %s", token));
     }
 
@@ -25,12 +29,16 @@ public class SpotifyData {
         return token;
     }
 
-    public static SpotifyApi getApi() { return  spotifyApi;}
-    public static String getClientId(){
+    public static SpotifyApi getApi() {
+        return spotifyApi;
+    }
+
+    public static String getClientId() {
         return CLIENT_ID;
     }
 
-    public static String getRedirectUri(){
+    public static String getRedirectUri() {
         return REDIRECT_URI;
     }
+
 }
