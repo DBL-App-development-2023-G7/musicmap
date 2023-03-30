da4b45e41389824282b3e38c8c83f8c2de39823d
TPGamesNL <pet.teun.03@gmail.com>
2023-03-30 22:19:41 +0200

    Reformat FirebaseTokenStorage

--- app/src/main/java/com/example/musicmap/util/spotify/FirebaseTokenStorage.java
+++ app/src/main/java/com/example/musicmap/util/spotify/FirebaseTokenStorage.java
@@ -2,27 +2,25 @@ package com.example.musicmap.util.spotify;
 
 import android.util.Log;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.HashMap;
-import java.util.Map;
+public class FirebaseTokenStorage {
 
-public class FirebaseTokenStorage{
-    private final static String REFRESH_TOKEN_FIELD = "refreshToken";
-    private static String userID;
+    private static final String REFRESH_TOKEN_FIELD = "refreshToken";
+
+    private final String userID;
 
     public FirebaseTokenStorage(String userId) {
         this.userID = userId;
     }
 
     public interface TokenReceivedCallback {
-        public void onComplete(String token);
+        void onComplete(String token);
     }
 
-    public void getRefreshToken(TokenReceivedCallback tokenRecievedCallback) {
+    public void getRefreshToken(TokenReceivedCallback tokenReceivedCallback) {
         FirebaseFirestore firestore = FirebaseFirestore.getInstance();
         DocumentReference docRef = firestore.collection("Users").document(userID);
         docRef.get().addOnCompleteListener(task -> {
@@ -30,7 +28,7 @@ public class FirebaseTokenStorage{
                 DocumentSnapshot document = task.getResult();
                 if (document.exists()) {
                     String refreshToken = document.getString(REFRESH_TOKEN_FIELD);
-                    tokenRecievedCallback.onComplete(refreshToken);
+                    tokenReceivedCallback.onComplete(refreshToken);
                 } else {
                     Log.d("debug", "[poop] No such document");
                 }
@@ -51,4 +49,5 @@ public class FirebaseTokenStorage{
                                 exception.getMessage()))
                 );
     }
+
 }
