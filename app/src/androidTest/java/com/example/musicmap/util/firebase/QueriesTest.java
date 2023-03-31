f7bb3f2ab75ac5f497980686c10ae6315970bd4a
Pradyuman7 <p.dixit@student.tue.nl>
2023-03-31 11:15:40 +0200

    Add some tests for Queries

--- app/src/androidTest/java/com/example/musicmap/util/firebase/QueriesTest.java
+++ app/src/androidTest/java/com/example/musicmap/util/firebase/QueriesTest.java
@@ -1,6 +1,11 @@
 package com.example.musicmap.util.firebase;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -9,20 +14,45 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+import java.util.concurrent.ExecutionException;
+
 public class QueriesTest {
 
-    private FirebaseFirestore firebaseFirestore;
+    private String userNameThatExistsInFirebase = "username";
+    private String userNameThatDoesNotExistsInFirebase = "some-username";
+    private String emailThatExistsInFirebase = "pradyumanreal7@gmail.com";
+    private String emailThatDoesnotExistInFirebase = "wrong@wrong";
 
-
-    @Before
-    public void setup() {
-        firebaseFirestore = FirebaseFirestore.getInstance();
+    @Test
+    public void testGetUsersByUsername_success_usernameExists() throws ExecutionException, InterruptedException {
+        Task<QuerySnapshot> task = Queries.getUsersWithUsername(userNameThatExistsInFirebase);
+        QuerySnapshot querySnapshot = Tasks.await(task);
+        assertFalse(querySnapshot.isEmpty());
+        assertEquals(querySnapshot.getDocuments().size(), 1);
+        assertEquals(querySnapshot.getDocuments().get(0).get("username"), userNameThatExistsInFirebase);
     }
 
     @Test
-    public void testGetUsersByUsername() {
-        Task<QuerySnapshot>
+    public void testGetUsersByUsername_success_usernameDoesNotExist() throws ExecutionException, InterruptedException {
+        Task<QuerySnapshot> task = Queries.getUsersWithUsername(userNameThatDoesNotExistsInFirebase);
+        QuerySnapshot querySnapshot = Tasks.await(task);
+        assertTrue(querySnapshot.isEmpty());
+    }
+
+    @Test
+    public void testGetUsersByUsername_success_emailExists() throws ExecutionException, InterruptedException {
+        Task<QuerySnapshot> task = Queries.getUserWithEmail(emailThatExistsInFirebase);
+        QuerySnapshot querySnapshot = Tasks.await(task);
+        assertFalse(querySnapshot.isEmpty());
+        assertEquals(querySnapshot.getDocuments().size(), 1);
+        assertEquals(querySnapshot.getDocuments().get(0).get("email"), emailThatExistsInFirebase);
+    }
+
+    @Test
+    public void testGetUsersByUsername_success_emailDoesNotExist() throws ExecutionException, InterruptedException {
+        Task<QuerySnapshot> task = Queries.getUserWithEmail(emailThatDoesnotExistInFirebase);
+        QuerySnapshot querySnapshot = Tasks.await(task);
+        assertTrue(querySnapshot.isEmpty());
     }
 
 }
