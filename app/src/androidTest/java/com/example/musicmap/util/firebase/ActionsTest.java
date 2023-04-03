3efe29531f1dc7d28d66d68e7a5738685600191b
Pradyuman7 <p.dixit@student.tue.nl>
2023-04-03 18:41:18 +0200

    Use DI for testing Actions

--- app/src/androidTest/java/com/example/musicmap/util/firebase/ActionsTest.java
+++ app/src/androidTest/java/com/example/musicmap/util/firebase/ActionsTest.java
@@ -1,62 +1,74 @@
 package com.example.musicmap.util.firebase;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.example.musicmap.feed.ConcertMemory;
 import com.example.musicmap.feed.MusicMemory;
 import com.example.musicmap.feed.Song;
 import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
+import com.google.android.gms.tasks.TaskCompletionSource;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import java.util.Date;
-import java.util.concurrent.CountDownLatch;
 
 public class ActionsTest {
 
-    private FirebaseFirestore firestore;
-    private CollectionReference usersCollectionRef;
-    private DocumentReference documentRef;
-
-    @Before
-    public void setup() {
-        firestore = Mockito.mock(FirebaseFirestore.class);
-        usersCollectionRef = Mockito.mock(CollectionReference.class);
-        documentRef = Mockito.mock(DocumentReference.class);
-    }
-
     @Test
-    public void postMusicMemory_success() throws InterruptedException {
-        String authorUid = "author-uid";
-        when(firestore.collection("Users")).thenReturn(usersCollectionRef);
-        when(usersCollectionRef.document(authorUid)).thenReturn(documentRef);
-        when(documentRef.collection("MusicMemories")).thenReturn(Mockito.mock(CollectionReference.class));
-        when(documentRef.collection("MusicMemories").add(any(MusicMemory.class)))
-                .thenAnswer(invocation -> Tasks.forResult(null));
-
+    public void postMusicMemory_success() {
         MusicMemory musicMemory = new MusicMemory("author-uid", new Date(),
                 new GeoPoint(10, 10), "https://imgur.com/photo", new Song(
                 "song", "1234", null, "https://imgur.com/photo-3", "https://spotify.com/preview"
         ));
-        Task<?> task = Actions.postMusicMemory(musicMemory);
 
-        CountDownLatch latch = new CountDownLatch(1);
-        task.addOnCompleteListener(result -> {
-            assertTrue(result.isSuccessful());
-            assertNull(result.getException());
-            latch.countDown();
-        });
 
-        latch.await();
+        FirebaseFirestore firestoreMock = mock(FirebaseFirestore.class);
+        CollectionReference collectionReferenceMock = mock(CollectionReference.class);
+        DocumentReference documentReferenceMock = mock(DocumentReference.class);
+        TaskCompletionSource<DocumentReference> taskCompletionSource = new TaskCompletionSource<>();
+
+        when(firestoreMock.collection(eq("Users"))).thenReturn(collectionReferenceMock);
+        when(collectionReferenceMock.document(eq(musicMemory.getAuthorUid()))).thenReturn(documentReferenceMock);
+        when(documentReferenceMock.collection(eq("MusicMemories"))).thenReturn(collectionReferenceMock);
+
+        when(collectionReferenceMock.add(eq(musicMemory))).thenReturn(taskCompletionSource.getTask());
+
+        Task<?> task = Actions.postMusicMemory(firestoreMock, musicMemory);
+        taskCompletionSource.setResult(documentReferenceMock);
+
+        task.getResult();
+        verify(collectionReferenceMock).add(eq(musicMemory));
+    }
+
+    @Test
+    public void postConcertMemory_success() {
+        ConcertMemory concertMemory = new ConcertMemory("author-uid", new Date(),
+                new GeoPoint(10, 10), "name", "https://youtube.com/video");
+
+
+        FirebaseFirestore firestoreMock = mock(FirebaseFirestore.class);
+        CollectionReference collectionReferenceMock = mock(CollectionReference.class);
+        DocumentReference documentReferenceMock = mock(DocumentReference.class);
+        TaskCompletionSource<DocumentReference> taskCompletionSource = new TaskCompletionSource<>();
+
+        when(firestoreMock.collection(eq("Users"))).thenReturn(collectionReferenceMock);
+        when(collectionReferenceMock.document(eq(concertMemory.getAuthorUid()))).thenReturn(documentReferenceMock);
+        when(documentReferenceMock.collection(eq("ConcertMemories"))).thenReturn(collectionReferenceMock);
+
+        when(collectionReferenceMock.add(eq(concertMemory))).thenReturn(taskCompletionSource.getTask());
+
+        Task<?> task = Actions.postConcertMemory(firestoreMock, concertMemory);
+        taskCompletionSource.setResult(documentReferenceMock);
+
+        task.getResult();
+        verify(collectionReferenceMock).add(eq(concertMemory));
     }
 
 }
--- app/src/main/java/com/example/musicmap/util/firebase/Actions.java
+++ app/src/main/java/com/example/musicmap/util/firebase/Actions.java
@@ -2,6 +2,8 @@ package com.example.musicmap.util.firebase;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import com.example.musicmap.feed.ConcertMemory;
 import com.example.musicmap.feed.MusicMemory;
 import com.google.android.gms.tasks.Task;
@@ -31,6 +33,26 @@ public class Actions {
     }
 
     /**
+     * Posts the music-memory for the {@code MusicMemory.authorUid}. Overloaded method mainly used
+     * for testing.
+     *
+     * @param firestore the firebaseFirestore reference for DI
+     * @param musicMemory the music-memory object
+     */
+    public static Task<?> postMusicMemory(@NonNull FirebaseFirestore firestore, MusicMemory musicMemory) {
+        String authorUid = musicMemory.getAuthorUid();
+
+        return firestore.collection("Users").document(authorUid)
+                .collection("MusicMemories").add(musicMemory).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Log.i(TAG, "Successfully created music memory with ID " + task.getResult().getId());
+                    } else {
+                        Log.e(TAG, "Could not create music memory", task.getException());
+                    }
+                });
+    }
+
+    /**
      * Posts the concert-memory for the {@code ConcertMemory.authorUid}.
      *
      * @param concertMemory the concert-memory object
@@ -39,6 +61,26 @@ public class Actions {
         String authorUid = concertMemory.getAuthorUid();
 
         FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        return firestore.collection("Users").document(authorUid)
+                .collection("ConcertMemories").add(concertMemory).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Log.i(TAG, "Successfully created concert memory with ID " + task.getResult().getId());
+                    } else {
+                        Log.e(TAG, "Could not create concert memory", task.getException());
+                    }
+                });
+    }
+
+    /**
+     * Posts the concert-memory for the {@code ConcertMemory.authorUid}. Overloaded method mainly used
+     * for testing.
+     *
+     * @param firestore the firebaseFirestore instance for DI
+     * @param concertMemory the concert-memory object
+     */
+    public static Task<?> postConcertMemory(@NonNull FirebaseFirestore firestore, ConcertMemory concertMemory) {
+        String authorUid = concertMemory.getAuthorUid();
+
         return firestore.collection("Users").document(authorUid)
                 .collection("ConcertMemories").add(concertMemory).addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
