5016c4b6c3737f97d3ff173dd2865cbeee428713
TPGamesNL <pet.teun.03@gmail.com>
2023-04-03 13:30:27 +0200

    Log username update exception when caught

--- app/src/main/java/com/example/musicmap/util/ui/ChangeUsernameDialogFragment.java
+++ app/src/main/java/com/example/musicmap/util/ui/ChangeUsernameDialogFragment.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +18,8 @@ import com.example.musicmap.util.firebase.AuthSystem;
 import com.example.musicmap.util.regex.InputChecker;
 
 public class ChangeUsernameDialogFragment extends DialogFragment {
+
+    private static final String TAG = "ChangerUsernameDialogFormat";
 
     private EditText newUsernameInput;
 
@@ -60,6 +63,7 @@ public class ChangeUsernameDialogFragment extends DialogFragment {
                             this.dismiss();
                         } else {
                             Exception exception = task.getException();
+                            Log.e(TAG, "Exception occurred while changing username", exception);
                             if (exception != null) {
                                 String message = exception.getMessage();
                                 newUsernameInput.setError(message);
