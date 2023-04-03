9bc11a7a6c164290448014684055cf2d2a52fe05
TPGamesNL <pet.teun.03@gmail.com>
2023-04-03 11:55:09 +0200

    Add method to FragmentUtil for providing arguments Bundle

--- app/src/main/java/com/example/musicmap/util/ui/FragmentUtil.java
+++ app/src/main/java/com/example/musicmap/util/ui/FragmentUtil.java
@@ -1,7 +1,10 @@
 package com.example.musicmap.util.ui;
 
+import android.os.Bundle;
+
 import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -39,6 +42,24 @@ public class FragmentUtil {
         // TODO don't replace if that fragment is already in the container
         fragmentManager.beginTransaction().setReorderingAllowed(true).replace(containerID,
                 fragmentClass, null).commit();
+    }
+
+    /**
+     * This method replaces the current fragment inside the given container with the given
+     * fragment class.
+     *
+     * @param fragmentManager the given Fragment Manager
+     * @param containerID     the id of the container
+     * @param fragmentClass   the class of the fragment
+     * @param args            the arguments for the fragment
+     */
+    public static void replaceFragment(@NonNull FragmentManager fragmentManager,
+                                       @IdRes int containerID,
+                                       @NonNull Class<? extends Fragment> fragmentClass,
+                                       @Nullable Bundle args) {
+        // TODO don't replace if that fragment is already in the container
+        fragmentManager.beginTransaction().setReorderingAllowed(true).replace(containerID,
+                fragmentClass, args).commit();
     }
 
 }
