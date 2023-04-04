189a4d47d5a9380eee05f3ede4dd5f4dd22f614a
Pradyuman7 <p.dixit@student.tue.nl>
2023-04-04 07:38:52 +0200

    Implement PopularSongsAdapter and MostPlayedSongsFragment

--- app/src/main/java/com/example/musicmap/screens/artist/PopularSongsAdapter.java
+++ app/src/main/java/com/example/musicmap/screens/artist/PopularSongsAdapter.java
@@ -1,4 +1,53 @@
 package com.example.musicmap.screens.artist;
 
-public class PopularSongsAdapter {
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.musicmap.R;
+import com.example.musicmap.feed.Song;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class PopularSongsAdapter extends ArrayAdapter<Song> {
+
+    private final Activity activityContext;
+
+    public PopularSongsAdapter(@NonNull Activity activityContext, int resource, @NonNull List<Song> songs) {
+        super(activityContext, resource, songs);
+        this.activityContext = activityContext;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, ViewGroup parent) {
+        View row = convertView;
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(activityContext);
+            row = inflater.inflate(R.layout.song_layout_artist_data, parent, false);
+        }
+
+        ImageView songImage = row.findViewById(R.id.popular_song_image);
+        TextView songName = row.findViewById(R.id.popular_song_name);
+        TextView streamDetails = row.findViewById(R.id.popular_song_count);
+
+        Song song = getItem(position);
+        if (song != null) {
+            songName.setText(song.getName());
+            streamDetails.setText(song.getSpotifyAristId());
+            Picasso.get().load(song.getImageUri()).into(songImage);
+        }
+
+        return row;
+    }
+
 }
--- app/src/main/java/com/example/musicmap/screens/main/ArtistDataMostPlayedSongsFragment.java
+++ app/src/main/java/com/example/musicmap/screens/main/ArtistDataMostPlayedSongsFragment.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 
 import com.example.musicmap.R;
+import com.example.musicmap.screens.artist.PopularSongsAdapter;
+
+import java.util.ArrayList;
 
 public class ArtistDataMostPlayedSongsFragment extends MainFragment {
 
@@ -17,6 +21,10 @@ public class ArtistDataMostPlayedSongsFragment extends MainFragment {
         View mostPlayedSongsView = inflater.inflate(R.layout.fragment_most_played_songs, container, false);
         Activity activity = requireActivity();
 
+        PopularSongsAdapter popularSongsAdapter = new PopularSongsAdapter(activity, R.layout.song_layout_artist_data,
+                new ArrayList<>());
+        ListView feedListView = mostPlayedSongsView.findViewById(R.id.most_played_list);
+        feedListView.setAdapter(popularSongsAdapter);
 
         return mostPlayedSongsView;
     }
