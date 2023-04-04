85bb4bd9b28079027b9b005f5dec5fe52674295f
viliamDokov <viliamDokov@users.noreply.github.com>
2023-04-04 10:24:38 +0200

    Post Screen and Spotfy API (#40)
    * Created basic layout for the post screen
    
    * Improved create music memory UI
    
    * Created First Implemention of image Capture!
    
    * Cleaned up code in PostFragment and CameraActivity
    
    * Now Fetching latest song from spotify
    
    * Added a spotify Session class (will be changed)
    
    * Added a spotify Session class (will be changed)
    
    * Add spotify APi utilities
    
    * Added a primitive search bar (with a lot of bugs)
    
    * Cleaned up some bugs in SongSerch and added some documentation (still a lot to do)
    
    * Improved search result view (slightly)
    
    * Add Song class
    
    * Implement Song storing in MusicMemory
    
    * Fixed NullPointerException is SerachBar if Play history is empty
    
    * Chenged Autentication to Browser only
    
    * Reverted back to App authentication
    
    * Comit to preserve some changes (I know I can stash but no)
    
    * Added PKCE support in SpotifyAuthActivityh
    
    * Added PKCE storage in firebase
    
    * Finished (roughly) all post fragment functions
    
    * Fixed wrong image rotation bug
    
    * Fix tests compilation errors since MusicMemory changes
    
    * Delete app-debug.apk and output-metadata.json
    
    * Some formatting changes in non-Java files
    
    * Add app/debug/* to .gitignore
    
    * Remove SessionAndInternetListenerActivity#getSession, replace with Session#getInstance
    
    * Reformat CameraActivity
    
    * Remove unused private method CameraActivity#setupCamera
    
    * Remove unnecessary requireActivity from FeedFragment feed loading failure message getting
    
    * Remove unused imports HomeActivity
    
    * Remove TODO message HomeActivity
    
    * Formatting changes PostFragment
    
    * Reformat SearchFragment
    
    * Undo unnecessary change CameraPermission
    
    * Reformat FirebaseTokenStorage
    
    * Reformat SpotifyAuthActivity
    
    * Reformat SpotifyData
    
    * Reformat SpotifySession
    
    * Make SpotifySession Deprecated
    
    * Reformat SpotifySongAdapter
    
    * Reformat SpotifyUtils
    
    * Fixed a null Pointer exception on a cancelled camera activity
    
    * Fixed camera image download URL from firebase
    
    * Removed unneeded spotify api libraries and the deprecated spotify session class
    
    * Cleaned up code for fetching user location
    
    * Start cleaning up PostFragment
    
    * Moved the ugly code in spotify searchBar to spotify utils
    
    * More cleanup for PostFragment
    
    * Move firebase upload to Actions and refactor PostFragment a bit
    
    * Fix the picasso thread error
    
    * Minor cleanup
    
    * Remove unused imports
    
    * Make PostFragment#cameraActivityResultLauncher private
    
    * Fix remaining checkstyle
    
    * Fixed some checkstyle indentation issues
    
    * Purified SpotifyAuthActivity
    
    * More cleaning in the SpotifyAuthActivity
    
    * Cleaned up FirebaseTokenStorage and SpotifyAuthActivity
    
    * More cleaning
    
    * Finished cleaning stuff up
    
    ---------
    
    Co-authored-by: TPGamesNL <pet.teun.03@gmail.com>
    Co-authored-by: Pradyuman7 <p.dixit@student.tue.nl>
    Co-authored-by: TPGamesNL <29547183+TPGamesNL@users.noreply.github.com>
    Co-authored-by: Andrei Tudor Popescu <a.popescu1@student.tue.nl>

---
+++ app/src/main/java/com/example/musicmap/util/spotify/SpotifyData.java
@@ -0,0 +1,46 @@
+package com.example.musicmap.util.spotify;
+
+import android.util.Log;
+
+import se.michaelthelin.spotify.SpotifyApi;
+
+/**
+ * A storage class which stores some global variables.
+ *
+ * Using static classes is not ideal but it is easy to do.
+ */
+public class SpotifyData {
+
+    //TODO this has to be moved in another file
+    private static final String CLIENT_ID = "56ab7fed83514a7a96a7b735737280d8";
+    private static final String REDIRECT_URI = "musicmap://spotify-auth";
+
+    private static String token = null;
+    private static SpotifyApi spotifyApi;
+
+    public static void setToken(String inputToken) {
+        token = inputToken;
+        spotifyApi = new SpotifyApi.Builder()
+                .setAccessToken(token)
+                .build();
+
+        Log.d("debug", String.format("[poop] token recieved %s", token));
+    }
+
+    public static String getToken() {
+        return token;
+    }
+
+    public static SpotifyApi getApi() {
+        return spotifyApi;
+    }
+
+    public static String getClientId() {
+        return CLIENT_ID;
+    }
+
+    public static String getRedirectUri() {
+        return REDIRECT_URI;
+    }
+
+}
