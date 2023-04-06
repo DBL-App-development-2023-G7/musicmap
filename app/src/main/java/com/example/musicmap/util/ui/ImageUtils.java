67c4dc290e078326bbab1a1c974cf63f2dd1005d
TPGamesNL <pet.teun.03@gmail.com>
2023-04-06 21:18:51 +0200

    Fix checkstyle, redo log messages

--- app/src/main/java/com/example/musicmap/util/ui/ImageUtils.java
+++ app/src/main/java/com/example/musicmap/util/ui/ImageUtils.java
@@ -12,9 +12,12 @@ import androidx.exifinterface.media.ExifInterface;
 import java.io.IOException;
 
 public class ImageUtils {
+
     private static final String TAG = "ImageUtils";
+
     public static float getImageRotationFromEXIF(Activity activity, Uri imageUri) throws IOException {
-        Log.w(TAG, String.format("Start: %s", imageUri.toString()));
+        Log.d(TAG, "Getting rotation of " + imageUri.toString());
+
         float rotation = 0f;
         ExifInterface exifInterface = new ExifInterface(activity.getContentResolver().openInputStream(imageUri));
         int orientation = exifInterface.getAttributeInt(TAG_ORIENTATION, ORIENTATION_NORMAL);
@@ -35,8 +38,10 @@ public class ImageUtils {
             default:
                 break;
         }
-        Log.w(TAG, String.format("End: %s", imageUri.toString()));
-        Log.w(TAG, String.format("End: .2%f", rotation));
+
+        Log.d(TAG, String.format("End: .2%f", rotation));
+
         return rotation;
     }
+
 }
