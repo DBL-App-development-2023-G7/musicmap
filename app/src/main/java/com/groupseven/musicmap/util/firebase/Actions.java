bac13ea8cc2ff93a1f805ea32bf35a857a6fef2a
TPGamesNL <pet.teun.03@gmail.com>
2023-04-15 14:50:51 +0200

    Avoid path collisions in uploadMusicMemoryImage

--- app/src/main/java/com/groupseven/musicmap/util/firebase/Actions.java
+++ app/src/main/java/com/groupseven/musicmap/util/firebase/Actions.java
@@ -4,17 +4,22 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.util.Log;
 
-import com.groupseven.musicmap.models.MusicMemory;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.groupseven.musicmap.models.MusicMemory;
 import com.groupseven.musicmap.util.TaskUtil;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
 
 public class Actions {
 
@@ -48,22 +53,58 @@ public class Actions {
      * @return a future containing a {@link Uri} for downloading the image.
      */
     public static CompletableFuture<Uri> uploadMusicMemoryImage(Bitmap capturedImage, String authorUid) {
+        // Convert the image to JPEG
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         capturedImage.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
         byte[] data = outputStream.toByteArray();
 
+        // Store the JPEG image under the author UID with a random UUID
         StorageReference rootReference = FirebaseStorage.getInstance().getReference();
-        String uuid = UUID.randomUUID().toString();
-        StorageReference imageRef = rootReference.child(String.format("users/%s/memories/%s.jpg", authorUid, uuid));
-        UploadTask uploadTask = imageRef.putBytes(data);
 
-        return TaskUtil.getFuture(uploadTask)
-                .thenCompose(unused -> TaskUtil.getFuture(imageRef.getDownloadUrl()))
-                .whenComplete((url, throwable) -> {
-                    if (throwable != null) {
-                        Log.e(TAG, "Could not upload the image", throwable);
+        // Run the whole thing on a separate thread, so we can simply wait for a task to be done
+        return CompletableFuture.supplyAsync(() -> {
+            // Generate a path in the storage that does not exist already (avoid collisions)
+            StorageReference imageRef;
+            boolean pathExists;
+            do {
+                // Generate a random UUID and form the storage path with it
+                String uuid = UUID.randomUUID().toString();
+                imageRef = rootReference.child(String.format("users/%s/memories/%s.jpg", authorUid, uuid));
+
+                Log.d(TAG, "Checking if image '" + imageRef.getPath() + "' exists");
+
+                /*
+                Firebase storage doesn't have a way to check if a StorageReference exists, see
+                https://github.com/flutter/flutter/issues/18315
+
+                Therefore, try to get the download url and catch the exception if the path doesn't exist.
+
+                This will still print an exception to console each time it finds a path that doesn't exist,
+                but at least it avoids collisions.
+                 */
+                try {
+                    TaskUtil.getFuture(imageRef.getDownloadUrl()).join();
+
+                    pathExists = true;
+                } catch (CompletionException e) {
+                    // If we get an exception other than the expected one, we should still fail
+                    if (!(e.getCause() instanceof IOException)) {
+                        throw e;
                     }
-                });
+
+                    Log.w(TAG, "You can ignore the exception above, this is expected behavior; "
+                            + "see Actions#uploadMusicMemoryImage");
+                    pathExists = false;
+                }
+            } while (pathExists);
+
+            // Start uploading and get download url
+            Log.d(TAG, "Started uploading image (" + data.length + " bytes)");
+            TaskUtil.getFuture(imageRef.putBytes(data)).join();
+            Log.d(TAG, "Uploading image");
+
+            return TaskUtil.getFuture(imageRef.getDownloadUrl()).join();
+        });
     }
 
 }
