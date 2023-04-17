31c45a0f487356be83fba8333c8f1300099af8a2
Pradyuman7 <p.dixit@student.tue.nl>
2023-04-17 09:31:06 +0200

    Add more tests for methods from InputChecker

--- app/src/androidTest/java/com/groupseven/musicmap/util/regex/InputCheckerTest.java
+++ app/src/androidTest/java/com/groupseven/musicmap/util/regex/InputCheckerTest.java
@@ -56,8 +56,6 @@ public class InputCheckerTest {
         assertNull(emailInput.getError());
     }
 
-    // Similar tests for checkIdentifier, checkPassword, checkFirstName, checkLastName, and checkRepeatPassword
-
     @Test
     public void checkUsername_emptyInput() throws ExecutionException, InterruptedException {
         EditText usernameInput = new EditText(context);
@@ -89,6 +87,104 @@ public class InputCheckerTest {
         boolean check = result.get();
         assertFalse(check);
         assertEquals(context.getString(R.string.input_error_username_exists), usernameInput.getError());
+    }
+
+    @Test
+    public void testCheckIdentifier_empty() {
+        EditText input = new EditText(context);
+        boolean result = InputChecker.checkIdentifier("", input);
+        assertFalse(result);
+        assertEquals(context.getString(R.string.input_error_enter_email), input.getError().toString());
+    }
+
+    @Test
+    public void testCheckIdentifier_invalidFormat() {
+        EditText input = new EditText(context);
+        boolean result = InputChecker.checkIdentifier("invalid-username", input);
+        assertFalse(result);
+        assertEquals(context.getString(R.string.input_error_valid_email), input.getError().toString());
+    }
+
+    @Test
+    public void testCheckIdentifier_valid() {
+        EditText input = new EditText(context);
+        boolean result = InputChecker.checkIdentifier("valid-email@example.com", input);
+        assertTrue(result);
+        assertNull(input.getError());
+    }
+
+    @Test
+    public void testCheckPassword_empty() {
+        EditText input = new EditText(context);
+        boolean result = InputChecker.checkPassword("", input);
+        assertFalse(result);
+        assertEquals(context.getString(R.string.input_error_enter_password), input.getError().toString());
+    }
+
+    @Test
+    public void testCheckPassword_invalidFormat() {
+        EditText input = new EditText(context);
+        boolean result = InputChecker.checkPassword("short", input);
+        assertFalse(result);
+        assertEquals(context.getString(R.string.input_error_valid_password), input.getError().toString());
+    }
+
+    @Test
+    public void testCheckPassword_valid() {
+        EditText input = new EditText(context);
+        boolean result = InputChecker.checkPassword("validpassword123", input);
+        assertTrue(result);
+        assertNull(input.getError());
+    }
+
+    @Test
+    public void testCheckFirstName_empty() {
+        EditText input = new EditText(context);
+        boolean result = InputChecker.checkFirstName("", input);
+        assertFalse(result);
+        assertEquals(context.getString(R.string.input_error_enter_first_name), input.getError().toString());
+    }
+
+    @Test
+    public void testCheckFirstName_valid() {
+        EditText input = new EditText(context);
+        boolean result = InputChecker.checkFirstName("John", input);
+        assertTrue(result);
+        assertNull(input.getError());
+    }
+
+    @Test
+    public void testCheckLastName_empty() {
+        EditText input = new EditText(context);
+        boolean result = InputChecker.checkLastName("", input);
+        assertFalse(result);
+        assertEquals(context.getString(R.string.input_error_enter_last_name), input.getError().toString());
+    }
+
+    @Test
+    public void testCheckLastName_valid() {
+        EditText input = new EditText(context);
+        boolean result = InputChecker.checkLastName("Doe", input);
+        assertTrue(result);
+        assertNull(input.getError());
+    }
+
+    @Test
+    public void testCheckRepeatPassword_matchingPasswords() {
+        String password = "password123";
+        String repeatPassword = "password123";
+        EditText repeatPasswordInput = new EditText(context);
+        assertTrue(InputChecker.checkRepeatPassword(repeatPassword, password, repeatPasswordInput));
+        assertNull(repeatPasswordInput.getError());
+    }
+
+    @Test
+    public void testCheckRepeatPassword_nonMatchingPasswords() {
+        String password = "password123";
+        String repeatPassword = "password456";
+        EditText repeatPasswordInput = new EditText(context);
+        assertFalse(InputChecker.checkRepeatPassword(repeatPassword, password, repeatPasswordInput));
+        assertEquals(context.getString(R.string.input_error_passwords_not_matching), repeatPasswordInput.getError().toString());
     }
 
 }
