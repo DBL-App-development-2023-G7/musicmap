7bccad949cf47b8a0d3800b234ddd853a3583d13
TPGamesNL <pet.teun.03@gmail.com>
2023-04-20 14:57:14 +0200

    Fix checkstyle

--- app/src/main/java/com/groupseven/musicmap/spotify/SpotifyAccess.java
+++ app/src/main/java/com/groupseven/musicmap/spotify/SpotifyAccess.java
@@ -25,7 +25,6 @@ public final class SpotifyAccess {
             .setRedirectUri(SpotifyHttpManager.makeUri(Constants.SPOTIFY_REDIRECT_URI))
             .build();
 
-
     /**
      * The {@link SpotifyAccess} instance.
      */
