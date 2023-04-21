bb987185131f45e5e6384268d53add08d721c929
TPGamesNL <pet.teun.03@gmail.com>
2023-04-21 13:03:43 +0200

    Cleanup, add Javadocs & comments, reformat and refactor PostFragment

--- app/src/main/java/com/groupseven/musicmap/screens/main/musicmemory/create/PostFragment.java
+++ app/src/main/java/com/groupseven/musicmap/screens/main/musicmemory/create/PostFragment.java
@@ -35,10 +35,10 @@ import com.groupseven.musicmap.models.MusicMemory;
 import com.groupseven.musicmap.models.Song;
 import com.groupseven.musicmap.screens.main.MainFragment;
 import com.groupseven.musicmap.screens.main.feed.FeedFragment;
+import com.groupseven.musicmap.spotify.SpotifyAccess;
 import com.groupseven.musicmap.util.firebase.Actions;
 import com.groupseven.musicmap.util.permissions.CameraPermission;
 import com.groupseven.musicmap.util.permissions.LocationPermission;
-import com.groupseven.musicmap.spotify.SpotifyAccess;
 import com.groupseven.musicmap.util.ui.FragmentUtil;
 import com.groupseven.musicmap.util.ui.ImageUtils;
 import com.groupseven.musicmap.util.ui.Message;
@@ -52,6 +52,8 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import javax.annotation.Nullable;
+
 public class PostFragment extends MainFragment {
 
     private static final String TAG = "PostFragment";
@@ -61,22 +63,31 @@ public class PostFragment extends MainFragment {
      */
     public static final String SONG_RESULT_KEY = "searchSong";
 
+    // Permissions for location and camera
     private final LocationPermission locationPermission = new LocationPermission(this);
     private final CameraPermission cameraPermission = new CameraPermission(this);
+
+    @Nullable
     private FusedLocationProviderClient fusedLocationClient;
 
-    private Session currentSession;
+    /**
+     * The parent activity hosting this fragment.
+     */
     private SessionListenerActivity parentActivity;
+
     private ImageView songImageView;
     private Button addSongButton;
     private Button addLocationButton;
     private ImageView capturedImagePreview;
     private Button postMemoryButton;
+
+    // Data for switching between search and post fragment
+    private PostViewModel model;
     private boolean shouldClearData = true;
 
-    private PostViewModel model;
-
-    // a launcher that launches the camera activity and handles the result
+    /**
+     * Launcher that launches the camera activity and handles the result
+     */
     private final ActivityResultLauncher<Intent> cameraActivityResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
@@ -87,8 +98,11 @@ public class PostFragment extends MainFragment {
                         Log.w(TAG, "Activity result from CameraActivity is null");
                         return;
                     }
+
                     Uri imageUri = resultIntent.getData();
                     Log.d(TAG, "Camera Activity result is called, URI: " + imageUri);
+
+                    // Load image preview
                     try {
                         Picasso.get().load(imageUri)
                                 .rotate(ImageUtils.getImageRotationFromEXIF(parentActivity, imageUri))
@@ -100,10 +114,13 @@ public class PostFragment extends MainFragment {
             }
     );
 
-    private SpotifyAccess spotifyAccess;
-
-    // this is a Picasso target into which Picasso will load the image taken from the camera
-    // in a field so it won't be garbage collected
+    /**
+     * The {@link Target} used to load the image into the {@link PostViewModel}.
+     * <p>
+     * This must be a field, as otherwise Picasso allows it to be garbage collected.
+     * According to them, this behaviour is a
+     * <a href="https://github.com/square/picasso/issues/1667#issuecomment-327198003">feature, not a bug</a>.
+     */
     private final Target cameraImageTarget = new Target() {
         @Override
         public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
@@ -125,13 +142,13 @@ public class PostFragment extends MainFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.currentSession = Session.getInstance();
+
         this.parentActivity = (SessionListenerActivity) requireActivity();
-        this.spotifyAccess = SpotifyAccess.getSpotifyAccessInstance();
 
         locationPermission.forceRequest();
         cameraPermission.forceRequest();
 
+        // Initiate fusedLocationClient, first check if Google Play Services is available
         if (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(parentActivity)
                 == ConnectionResult.SUCCESS) {
             fusedLocationClient = LocationServices.getFusedLocationProviderClient(parentActivity);
@@ -142,9 +159,12 @@ public class PostFragment extends MainFragment {
 
         model = new ViewModelProvider(parentActivity).get(PostViewModel.class);
 
+        // Start fetching user's location
         fetchUserLocation();
 
+        // Start fetching currently playing song
         model.setSongToCurrentUserSong();
+
         // Setup callback from search result fragment
         getParentFragmentManager().setFragmentResultListener(
                 SONG_RESULT_KEY,
@@ -181,7 +201,8 @@ public class PostFragment extends MainFragment {
         postMemoryButton = rootView.findViewById(R.id.postMemoryButton);
         postMemoryButton.setOnClickListener(view -> postMusicMemory());
 
-        spotifyAccess.refreshToken(new SpotifyAccess.TokenCallback() {
+        // Check if Spotify is connected
+        SpotifyAccess.getSpotifyAccessInstance().refreshToken(new SpotifyAccess.TokenCallback() {
             @Override
             public void onValidToken() {
                 postMemoryButton.setEnabled(true);
@@ -197,16 +218,10 @@ public class PostFragment extends MainFragment {
         return rootView;
     }
 
-    private void showUserLocation(Location location){
-        if (location != null) {
-            String locationText = getLocationText(location);
-            addLocationButton.setText(locationText);
-        }
-    }
-
     @Override
     public void onStop() {
         super.onStop();
+        // Reset the data
         if (shouldClearData) {
             model.clearData();
         }
@@ -214,13 +229,23 @@ public class PostFragment extends MainFragment {
         shouldClearData = true;
     }
 
-    private void showCapturedImage(Bitmap capturedImage){
+    /**
+     * Updates the UI to display the given image preview.
+     *
+     * @param capturedImage the image to display.
+     */
+    private void showCapturedImage(Bitmap capturedImage) {
         if (capturedImage != null) {
             capturedImagePreview.setImageBitmap(capturedImage);
             capturedImagePreview.setVisibility(View.VISIBLE);
         }
     }
 
+    /**
+     * Updates the UI to display the given song.
+     *
+     * @param song the song to display.
+     */
     private void showSelectedSong(Song song) {
         if (song != null) {
             songImageView.setVisibility(View.VISIBLE);
@@ -229,25 +254,75 @@ public class PostFragment extends MainFragment {
         }
     }
 
-    private void goToSearchFragment() {
-        shouldClearData = false;
-        FragmentUtil.replaceFragment(requireActivity().getSupportFragmentManager(), R.id.fragment_view,
-                SearchFragment.class);
+    /**
+     * Updates the UI with the given location.
+     *
+     * @param location the location to display.
+     */
+    private void showUserLocation(Location location) {
+        if (location != null) {
+            String locationText = getLocationText(location);
+            addLocationButton.setText(locationText);
+        }
     }
 
-    private void goToCameraActivity() {
-        shouldClearData = false;
-        Intent cameraIntent = new Intent(this.parentActivity, CameraActivity.class);
-        cameraActivityResultLauncher.launch(cameraIntent);
+    /**
+     * Converts a location to a human readable string.
+     * <p>
+     * This method may block.
+     *
+     * @param location the location to convert.
+     * @return the string describing the location.
+     */
+    private String getLocationText(Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        // A backup string of we cannot get a proper string format for the location
+        String coordinateString = String.format("Lat: %s \nLon: %s",
+                location.getLatitude(),
+                location.getLongitude()
+        );
+
+        Geocoder geocoder = new Geocoder(parentActivity);
+        try {
+            // Fetch the addresses
+            List<Address> addressList = geocoder.getFromLocation(latitude, longitude, 1);
+            if (addressList.isEmpty()) {
+                Log.w(TAG, "Geocoder could not find an associated address");
+                return coordinateString;
+            }
+
+            Address address = addressList.get(0);
+            List<String> addressFeatures = new ArrayList<>();
+
+            // Add some address features
+            addressFeatures.add(address.getThoroughfare());
+            addressFeatures.add(address.getLocality());
+            addressFeatures.add(address.getCountryName());
+
+            // Select the 2 most prominent features of the location,
+            //  filtering out unknown values
+            return addressFeatures.stream()
+                    .filter(Objects::nonNull)
+                    .limit(2)
+                    .collect(Collectors.joining(", "));
+        } catch (IOException e) {
+            Log.w(TAG, "Something went wrong while using Geocoder", e);
+            return coordinateString;
+        }
     }
 
-    // TODO move to a utilities class (I tried but it is a huge pain please help)
+    /**
+     * Fetches the user's location, and adds it to the {@link #model}.
+     */
     @SuppressLint("MissingPermission")
     private void fetchUserLocation() {
         if (fusedLocationClient == null) {
             Message.showFailureMessage(parentActivity, getString(R.string.gps_required));
             return;
         }
+
         if (locationPermission.isCoarseGranted() && locationPermission.isFineGranted()) {
             fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null)
                     .addOnSuccessListener(parentActivity, location -> {
@@ -255,6 +330,8 @@ public class PostFragment extends MainFragment {
                             Log.i(TAG, "Location unknown");
                             return;
                         }
+
+                        // Add location to model
                         model.getUserLocation().setValue(location);
                     })
                     .addOnFailureListener(parentActivity, exception ->
@@ -265,43 +342,27 @@ public class PostFragment extends MainFragment {
         }
     }
 
-    // Takes a location and returns a human readable string to display on the search bar
-    private String getLocationText(Location location){
-        double lat = location.getLatitude();
-        double lon = location.getLongitude();
-
-        String resultString = String.format("Lat: %s \nLon: %s",
-                location.getLatitude(),
-                location.getLongitude()
-        );
-        Geocoder geocoder = new Geocoder(parentActivity);
-        try {
-            List<Address> addressList = geocoder.getFromLocation(lat, lon, 1);
-            if (addressList.size() == 0) {
-                Log.w(TAG, "Geocoder could not find an associated address");
-                return resultString;
-            }
-
-            Address address = addressList.get(0);
-            List<String> addressFeatures = new ArrayList<>();
-
-            addressFeatures.add(address.getThoroughfare());
-            addressFeatures.add(address.getLocality());
-            addressFeatures.add(address.getCountryName());
-
-            resultString = addressFeatures.stream()
-                    .filter(Objects::nonNull)
-                    .limit(2)
-                    .collect(Collectors.joining(", "));
-
-            return resultString;
-
-        } catch (IOException e) {
-            Log.w(TAG, "Something went wrong while using Geocoder", e);
-            return resultString;
-        }
+    /**
+     * Open the song search fragment so the user can select the song.
+     */
+    private void goToSearchFragment() {
+        shouldClearData = false;
+        FragmentUtil.replaceFragment(requireActivity().getSupportFragmentManager(), R.id.fragment_view,
+                SearchFragment.class);
     }
 
+    /**
+     * Open the camera activity so the user can take a photo.
+     */
+    private void goToCameraActivity() {
+        shouldClearData = false;
+        Intent cameraIntent = new Intent(this.parentActivity, CameraActivity.class);
+        cameraActivityResultLauncher.launch(cameraIntent);
+    }
+
+    /**
+     * Posts the music memory that is stored in {@link #model}.
+     */
     private void postMusicMemory() {
         if (model.isSongNull()) {
             Message.showFailureMessage(this.parentActivity, getString(R.string.create_mm_track_required));
@@ -320,16 +381,17 @@ public class PostFragment extends MainFragment {
 
         postMemoryButton.setEnabled(false);
 
-        String authorID = this.currentSession.getCurrentUser().getUid();
+        String authorID = Session.getInstance().getCurrentUser().getUid();
         Bitmap capturedImage = model.getCameraImage().getValue();
 
-        Actions.uploadMusicMemoryImage(capturedImage, authorID).thenAccept(imageUrl ->
+        // Upload photo and music memory
+        Actions.uploadMusicMemoryImage(capturedImage, authorID).thenCompose(imageUrl ->
                 Actions.postMusicMemory(new MusicMemory(
-                    authorID,
-                    Calendar.getInstance().getTime(),
-                    model.getLocationAsGeoPoint(),
-                    imageUrl.toString(),
-                    model.getSelectedSong().getValue()
+                        authorID,
+                        Calendar.getInstance().getTime(),
+                        model.getLocationAsGeoPoint(),
+                        imageUrl.toString(),
+                        model.getSelectedSong().getValue()
                 )
         ).whenCompleteAsync((unused, throwable) -> {
             if (throwable != null) {
@@ -337,11 +399,13 @@ public class PostFragment extends MainFragment {
                 Message.showFailureMessage(this.parentActivity, getString(R.string.create_mm_failure));
                 postMemoryButton.setEnabled(true);
             } else {
+                // Return back to FeedFragment
                 FragmentUtil.replaceFragment(
                         requireActivity().getSupportFragmentManager(),
                         R.id.fragment_view,
                         FeedFragment.class
                 );
+
                 model.clearData();
                 Message.showSuccessMessage(this.parentActivity, getString(R.string.create_mm_success));
             }
